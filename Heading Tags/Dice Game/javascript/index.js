db8b394cdc138515313dccebc76d3848a66464df
Felix Kurniawan Dharma <felix.dharma121299@gmail.com>
2025-04-21 08:17:11 +0700

    Add to stash

--- Heading Tags/Dice Game/javascript/index.js
+++ Heading Tags/Dice Game/javascript/index.js
@@ -1,22 +1,31 @@
 function getDice() {
 
-    var getDice1 = Math.floor(Math.random() * 6 + 1);
-    var getDice2 = Math.floor(Math.random() * 6 + 1);
+    // var getDice1 = Math.floor(Math.random() * 6 + 1);
+    // var getDice2 = Math.floor(Math.random() * 6 + 1);
 
 
-    var hideElement1 = document.getElementsByClassName("dot-dice")[1];
-    var hideElement2 = document.getElementsByClassName("dot-dice")[4];
-    var hideElement3 = document.getElementsByClassName("dot-dice")[7];
-    hideElement1.className = "";
-    hideElement2.className = "";
-    hideElement3.className = "";
+    // var hideElement1 = document.getElementsByClassName("dot-dice")[1];
+    // var hideElement2 = document.getElementsByClassName("dot-dice")[4];
+    // var hideElement3 = document.getElementsByClassName("dot-dice")[7];
+    // hideElement1.className = "";
+    // hideElement2.className = "";
+    // hideElement3.className = "";
 
-    if (getDice1 === 2) {
-        var dot1 = document.getElementsByClassName("dot-dice")[0];
-        var dot2 = document.getElementsByClassName("dot-dice")[1];
-        var dot3 = document.getElementsByClassName("dot-dice")[4];
-        var dot4 = document.getElementsByClassName("dot-dice")[4];
-        dot1.className = "";
-        dot2.className = "";
-    }
+    // if (getDice1 === 2) {
+    //     var dot1 = document.getElementsByClassName("dot-dice")[0];
+    //     var dot2 = document.getElementsByClassName("dot-dice")[1];
+    //     var dot3 = document.getElementsByClassName("dot-dice")[3];
+    //     var dot4 = document.getElementsByClassName("dot-dice")[4];
+    //     var dot5 = document.getElementsByClassName("dot-dice")[5];
+    //     var dot6 = document.getElementsByClassName("dot-dice")[8];
+    //     var dot7 = document.getElementsByClassName("dot-dice")[7];
+    //     dot1.className = "";
+    //     dot2.className = "";
+    //     dot3.className = "";
+    //     dot4.className = "";
+    //     dot5.className = "";
+    //     dot6.className = "";
+    //     dot7.className = "";
+        
+    // }
 }
