8b23e0cb47306b76e9833858e9bbe98242b7f2b1
Felix Kurniawan Dharma <felix.dharma121299@gmail.com>
2025-04-23 08:53:46 +0700

    Drum Kit Done

--- Heading Tags/Drum Kit Starting Files/index.js
+++ Heading Tags/Drum Kit Starting Files/index.js
@@ -3,10 +3,65 @@ var numberOfDrum = document.querySelectorAll(".drum").length
 for (var i = 0; i < numberOfDrum; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        var tomOne = new Audio("sounds/tom-1.mp3");
-        tomOne.play();
+
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+
     });
 
 }
 
+document.addEventListener("keypress", function (event) {
+    var key = event.key;
+    makeSound(key);
+    buttonAnimation(key);
+});
 
+
+function makeSound(key) {
+    
+    switch (key) {
+        case "w":
+            var tomOne = new Audio("sounds/tom-1.mp3");
+            tomOne.play();
+            break;
+        case "a":
+            var tomTwo = new Audio("sounds/tom-2.mp3");
+            tomTwo.play();
+            break;
+
+        case "s":
+            var tomThree = new Audio("sounds/tom-3.mp3");
+            tomThree.play();
+            break;
+        case "d":
+            var tomFour = new Audio("sounds/tom-4.mp3");
+            tomFour.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+        default:
+    }
+
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
+
+}
