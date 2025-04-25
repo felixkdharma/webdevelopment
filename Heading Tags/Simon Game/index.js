78ca6fb0019564d270fa4a784288f607f93226a1
Felix Kurniawan Dharma <felix.dharma121299@gmail.com>
2025-04-25 09:47:57 +0700

    Index JS Updated

--- Heading Tags/Simon Game/index.js
+++ Heading Tags/Simon Game/index.js
@@ -1,8 +1,31 @@
+$("document").keypress(function (event) {
+    var key = event.key.toLowerCase();
+
+    console.log(key);
+    
+})
+
 function clickButton(e) {
 
     var classButton = $(e).attr("class")
 
     if (classButton === "button r") {
+        $(e).fadeOut(200, function () {
+            $(e).fadeIn(200, function () {
+            })
+        })
+    } else if (classButton === "button g") {
+        $(e).fadeOut(200, function () {
+            $(e).fadeIn(200, function () {
+            })
+        })
+    }
+    else if (classButton === "button y") {
+        $(e).fadeOut(200, function () {
+            $(e).fadeIn(200, function () {
+            })
+        })
+    } else if (classButton === "button b") {
         $(e).fadeOut(200, function () {
             $(e).fadeIn(200, function () {
             })
