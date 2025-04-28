c0b75ae2e0c7a0c4e261912afacce7ff51f7e4be
felixkdharma <felix.dharma121299@gmail.com>
2025-04-28 22:42:32 +0700

    HTTP Requests Done

--- Heading Tags/Backend/3.2 HTTP Requests/index.js
+++ Heading Tags/Backend/3.2 HTTP Requests/index.js
@@ -1,19 +1,19 @@
 import express from "express";
+
 const app = express();
 const port = 3000;
 
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+  res.send(
+    "<h1> Welcome Felix Kurniawan Dharma! </h1>" +
+      "<button> <a href='/about'>About Me </a> </button>"
+  );
 });
 
 app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
-});
-
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
-});
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  res.send("<h2> This is all about Felix Kurniawan Dharma </h2>");
 });
