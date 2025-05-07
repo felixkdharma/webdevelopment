596c3d680e12e77020b189462ca5376ee16c5b32
Felix Kurniawan Dharma <felix.dharma121299@gmail.com>
2025-05-07 10:09:47 +0700

    Personal Blog Added

---
+++ Heading Tags/Personal Blog/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3001;
+
+app.use(express.static("views"));
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {data});
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+const data = [
+    {
+        user: "Benjamin09", post: ` I am really confused, why they are not using the same font for all the headings? 
+                                    If i were you, i would not do that so far. Its gonna break their heart.
+                                    Since i have moved to Jakarta, i feel kinda diffrent. My hometown not always so crowded and noisy`
+    },
+
+    {
+        user: "Susy_Smith", post: `I cannot wait for 9th of Mei 2025. There is a big event in Jakarta and i am so excited to celebrate it with my friends.
+                                   The event is organized by SAMA. SAMA is an event organizer that is veery popular in Jakarta. The crew is very profressional 
+                                   and they are so friendly.` 
+    },
+    {
+        user: "JohnDoe11", post: `I am really sad, because my parents gonna break up. How do you fill when your loved ones are not together anymore?`
+    }
+]
+
+// const posts = [` I am really confused, why they are not using the same font for all the headings? 
+//     If i were you, i would not do that so far. Its gonna break their heart.
+//     Since i have moved to Jakarta, i feel kinda diffrent. My hometown not always so crowded and noisy` ,
+
+//     `I cannot wait for 9th of Mei 2025. There is a big event in Jakarta and i am so excited to celebrate it with my friends.
+//     The event is organized by SAMA. SAMA is an event organizer that is veery popular in Jakarta. The crew is very profressional 
+//     and they are so friendly.` ,
+
+//     `I am really sad, because my parents gonna break up. How do you fill when your loved ones are not together anymore?`
+// ];
+
+// const users = ["Benjamin09", "Susy_Smith", "JohnDoe"];
---
+++ Heading Tags/Personal Blog/public/styles/js/popup.js
@@ -0,0 +1,9 @@
+// Fungsi untuk menampilkan pop-up
+function showPopup() {
+  document.getElementById("popup").style.display = "flex";
+}
+
+// Fungsi untuk menutup pop-up
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
