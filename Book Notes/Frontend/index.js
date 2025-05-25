d17d4ed6c58e4529f1db6f5512c6b6242b622b7d
felixkdharma <felix.dharma121299@gmail.com>
2025-05-25 20:17:22 +0700

    Home Login Added

--- Book Notes/Frontend/index.js
+++ Book Notes/Frontend/index.js
@@ -8,30 +8,52 @@ const apiUrl = "http://localhost:3001";
 
 app.use(express.static("public"));
 app.use(express.static("views"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
+  res.render("login.ejs");
+});
 
-    try {
-        // Get data from Database
-        const response = await axios.get(`${apiUrl}/api/getBooks`);
-        const result = response.data;
-        console.log(result);
+app.post("/home", async (req, res) => {
+  const name = req.body["username"];
+  console.log(name);
 
-        // Get Image Cover from API
-        const imageResponse = await axios.get("https://covers.openlibrary.org/b/id/12547191.json");
-        const imageResult = imageResponse.data;
-        console.log(imageResult);
+  try {
+    // Get User ID from Database
+    const response = await axios.post(`${apiUrl}/api/getUser`, {
+      username: name,
+    });
 
-        res.render("home.ejs", 
-            {books: result,
-             image: imageResult
-            });
-    } catch (error) {
-        console.log("Error fetching data");
+    const result = response.data;
+
+    if (result.length > 0) {
+      res.redirect("/home");
     }
-})
+  } catch (error) {
+    console.log("Error fetching user");
+  }
+});
 
-app.listen(port, ()=> {
-    console.log(`Server is running on http://localhost:${port}`);
-})
+app.get("/home", async (req, res) => {
+  try {
+    // Get data from Database
+    const response = await axios.get(`${apiUrl}/api/getBooks`);
+    const result = response.data;
+    console.log(result);
+
+    // Get Image Cover from API
+    const imageResponse = await axios.get(
+      "https://covers.openlibrary.org/b/id/12547191.json"
+    );
+    const imageResult = imageResponse.data;
+    console.log(imageResult);
+
+    res.render("home.ejs", { books: result, image: imageResult });
+  } catch (error) {
+    console.log("Error fetching data");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
